9491cf862dfaec201a7c2c84e1a6015241685caa
PSibashishPatra <psibashishpatra@gmail.com>
2023-11-20 16:55:55 +0530

    first commit

---
+++ hoisting.js
@@ -0,0 +1,34 @@
+let a = 10 ;
+console.log(a);
+
+console.log(b);
+var b = 100 ;
+console.log(b);
+var b = 20 ;
+console.log(b); 
+
+
+//Initialization can be done later after declaration
+let c ;
+console.log(c);
+c = 15 ;
+console.log(c);
+
+//Missing initializer in const declaration
+/*const d ;
+console.log(d);
+d = 25 ;
+console.log(d); */
+
+const e = 30 ;
+console.log(e);
+
+//reassigning value to a const variable gives a type error
+// e = 45 ;
+
+
+/*Types of error
+syntax error
+type error
+reference error(During the temporal dead zone)*/
+  
