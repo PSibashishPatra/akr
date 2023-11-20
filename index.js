d0a14e9089326088d1ec106c1c774ab54212f0ad
PSibashishPatra <psibashishpatra@gmail.com>
2023-11-20 15:14:11 +0530

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 // object definition
 const human = {
-    firstName: "Judah",
-    lastName: "Parker",
-    age: 26,
+    firstName: "Ram",
+    lastName: "Sharma",
+    age: 23,
   };
   
   // function definition
   function greet() {
+    
     const string = `My name is ${this.firstName} ${this.lastName}. I am ${this.age} years old.`;
+   
     console.log(string);
+  
   }
   
-  // passing object as this value in call() method
   greet.call(human);
