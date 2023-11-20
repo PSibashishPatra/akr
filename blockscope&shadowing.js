de309bd7e5c63ded8cad7830775ef052d003c450
PSibashishPatra <psibashishpatra@gmail.com>
2023-11-20 18:11:31 +0530

    first commit

---
+++ blockscope&shadowing.js
@@ -0,0 +1,24 @@
+if(1) { 
+    //compound statement or block
+    var x = 101 ;
+    console.log(x);
+
+}
+
+
+
+var a = 100 ;
+    let b = 200 ;
+    const c = 300 ;
+
+{
+    var a = 10 ;
+    let b = 20 ;
+    const c = 30 ;
+    console.log(a);
+    console.log(b); 
+    console.log(c);
+}
+console.log(a);
+console.log(b); 
+console.log(c);
