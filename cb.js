8fb22f9f71fdc75de5c520ec3c3b3805c16f3c80
PSibashishPatra <psibashishpatra@gmail.com>
2023-11-23 18:37:19 +0530

    first commit

---
+++ cb.js
@@ -0,0 +1,10 @@
+ 
+
+function attachEventListeners() {
+    let count = 0 ;
+    document.getElementById("clickme").addEventListener("click",function xyz(){
+        console.log("Button Clicked");
+     });
+
+}
+addEventListeners();
