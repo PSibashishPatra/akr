ff6e37f14ecbfb534d331f41dddebeaceea23fa6
PSibashishPatra <psibashishpatra@gmail.com>
2023-11-23 17:58:07 +0530

    first commit

---
+++ callback.js
@@ -0,0 +1,16 @@
+//callback function in javascript
+setTimeout(function() {
+    console.log("timer");
+},5000)
+
+
+
+function x(y){
+    console.log("x");
+    y();
+
+}
+x(function y(){
+    console.log("y");
+
+})
